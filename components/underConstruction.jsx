3ec890fac1c8ca3e2f01d1e0974ff188b2746588
YuvarajPG <yuvar2978@gmail.com>
2025-12-11 13:10:33 +0530

    fixed

--- components/underConstruction.jsx
+++ components/underConstruction.jsx
@@ -3,9 +3,9 @@ const underConstruction = () => {
     return (
         <>
             <div className="flex justify-center items-center min-h-screen">
-              <p className="md:text-5xl flex justify-center text-white min-[320px]:text-2xl min-[320px]:mb-55">
-                Under Construction 🚜👷🚧🏗️
-            </p>
+                <p className="md:text-5xl flex justify-center text-white min-[320px]:text-2xl min-[320px]:mb-55">
+                    Under Construction 🚜👷🚧🏗️
+                </p>
             </div>
         </>
     );
